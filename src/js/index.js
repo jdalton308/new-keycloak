296149df8ee1b9895d883cd6962504549848366a
jdalton <jdalton@lifetimefitness.com>
2017-07-20 16:37:10 -0600

    Formatting for the 'long-form'

--- src/js/index.js
+++ src/js/index.js
@@ -128,11 +128,13 @@ const Forms = (function() {
 
 const Inputs = (function() {
 
-	function setValidState(labelEl, isValid, e) {
+	function setValidState(targetEl, isValid) {
+		const labelEl = targetEl.parentElement;
+
 		if (labelEl.classList.contains('has-content')) {
 		  labelEl.classList.toggle('valid', isValid);
 
-		  if (e.target.classList.contains('touched')) {
+		  if (targetEl.classList.contains('touched')) {
 		  	labelEl.classList.toggle('error', !isValid);
 		  }
 		} else {
@@ -140,28 +142,46 @@ const Inputs = (function() {
 		}
 	}
 
-	function validateText(e) {
+	function validateName(e) {
 		const validTextRegex = /^[a-z ,.'-]+$/i;
-		setValidState(e.target.parentElement, validTextRegex.test(e.target.value), e);
+		setValidState(e.target, validTextRegex.test(e.target.value));
 	}
 
 	function validateEmail(e) {
 	  const validEmailRegex = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-	  setValidState(e.target.parentElement, validEmailRegex.test(e.target.value), e);
+	  setValidState(e.target, validEmailRegex.test(e.target.value));
 	}
 
 	function validatePassword(e) {
-		setValidState(e.target.parentElement, (e.target.value.length >= 8), e);
+		setValidState(e.target, (e.target.value.length >= 8));
 	}
 
-	function getValidator(type) {
+	function validateStreet(e) {
+		const validStreetRegex = /^\d+\s[A-z]+\s[A-z]+/g;
+		setValidState(e.target, validStreetRegex.test(e.target.value));
+	}
+
+	function hasContent(e) {
+		setValidState(e.target, !!e.target.value.length);
+	}
+
+	function getValidator(inputEl) {
+		const type = inputEl.getAttribute('type');
+		const name = inputEl.getAttribute('name');
+
+		switch (name) {
+			case 'address' :
+			case 'postal' :
+				return hasContent;
+		}
+
 		switch (type) {
 			case 'email' :
 				return validateEmail;
 			case 'password' :
 				return validatePassword;
 			default :
-				return validateText;
+				return validateName;
 		}
 	}
 
@@ -177,7 +197,7 @@ const Inputs = (function() {
 		const inputField = inputContainer.querySelector('input');
 		
 		if (inputField) {
-			const inputValidator = getValidator(inputField.getAttribute('type'));
+			const inputValidator = getValidator(inputField);
 
 			// Keyup events
 			inputField.addEventListener('keyup', (e) => {
